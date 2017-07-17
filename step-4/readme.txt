485c8816d6d9fa99363796d9451fd4311e0f4c6b
snmnmin <snmnmin@gmail.com>
2017-07-17 10:00:20 -0500

    loaded files

---
+++ step-4/readme.txt
@@ -0,0 +1,13 @@
+This assignment is for thread and scheduler. 
+
+In this assignment, I have modified files:
+
+scheduler.H/C
+simple_timer.H/C
+thread.H/C
+
+Two scheduler schema is implemented:
+FIFO - First In First Out
+Round-Robin
+
+To get more details, refer to the design document.
---
+++ step-4/source/scheduler.C
@@ -0,0 +1,129 @@
+/*
+ File: scheduler.C
+ 
+ Author:
+ Date  :
+ 
+ */
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "scheduler.H"
+#include "thread.H"
+#include "console.H"
+#include "utils.H"
+#include "assert.H"
+#include "simple_keyboard.H"
+
+/*--------------------------------------------------------------------------*/
+/* DATA STRUCTURES */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* CONSTANTS */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* FORWARDS */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* METHODS FOR CLASS   S c h e d u l e r  */
+/*--------------------------------------------------------------------------*/
+
+Queue::Queue() 
+  {
+      head = 0;
+      _size = 0;
+  }
+void Queue::enqueue(Thread *t) {
+    Node *temp = new Node(t);
+    if (head == 0) {
+        head = temp;
+    }else {
+	Node *p = head;
+	while (p->next) p = p->next;
+	   p->next = temp;
+	}
+    _size++;
+   } 
+Thread* Queue::dequeue() {
+    if (!head) return 0;
+    Node* p = head;
+    head = p->next;
+    Thread *t = p->thread;
+    delete p;
+    _size--;
+    return t;
+  }
+   
+void Queue::remove(Thread * thread){
+    Node* p = head;
+    Node* pre = 0;
+    while(p && p->thread->ThreadId() != thread->ThreadId()) {
+	pre = p;
+	p = p->next;
+    }
+    if (!p) return;
+    if (pre == 0) {
+	head = p->next;
+    }else{
+	pre->next = p->next;
+    }
+    delete p;
+    _size--;
+  }
+
+Scheduler::Scheduler() {
+  ready_queue = Queue();
+  round_robin = false;
+  timer = NULL;
+  Console::puts("Constructed Scheduler.\n");
+}
+
+void Scheduler::yield() {
+  if (ready_queue.isEmpty()) return;
+  Thread* thread = ready_queue.dequeue();
+
+  if (Machine::interrupts_enabled()) 
+	Machine::disable_interrupts();
+
+  if (round_robin && timer) {timer->reset_Ticks();}
+  Thread::dispatch_to(thread);
+}
+
+void Scheduler::resume(Thread * _thread) {
+  if (!Machine::interrupts_enabled()) 
+	Machine::enable_interrupts();
+  ready_queue.enqueue(_thread);
+}
+
+void Scheduler::add(Thread * _thread) {
+  ready_queue.enqueue(_thread);
+}
+
+void Scheduler::terminate(Thread * _thread) {
+   ready_queue.remove(_thread);
+}
+
+bool Scheduler::is_round_robin() {return round_robin;}
+
+bool Scheduler::set_round_robin() {round_robin = true;}
+
+bool Scheduler::unset_round_robin() {round_robin = false;}
+
+void Scheduler::addTimer(SimpleTimer* _timer) {timer = _timer;}
